58f7a39f5a33e4239c3671c4614117dc359a1f8a
Colin Boyle <colinboyle@protonmail.com>
2024-09-02 15:59:25 +0100

    Implemented delete functionality

--- src/list.cpp
+++ src/list.cpp
@@ -73,13 +73,16 @@ void List::add_item() {
 void List::delete_item() {
     cout << "\n\n\n\n";
     cout << "*** Delete item ***\n";
-    cout << "Select an item index number to delete\n";
+    cout << "Select an item index number to delete: ";
 
     if(list.size()) {
 
         for(unsigned int i = 0; i < list.size(); i++) {
             cout << i << ": " << list[i] << "\n";
         }
+        int choiceNum; 
+        cin >> choiceNum;
+        list.erase(list.begin()+choiceNum);
     }
     else {
         cout << "No items in the list to delete\n";
